02d492d0768e8995cdeb1345e943c8b4e8aa66f4
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-09-13 20:02:30 -0400

    flexbox replaced calendar grid for responsive design

--- src/days.js
+++ src/days.js
@@ -14,3 +14,63 @@ export const days = [
         date: "2023-09-15"
     }
 ]
+
+export const september = [
+    {
+        day: '1',
+        date: "2023-09-13"
+    },
+    {
+        day: '2',
+        date: "2023-09-14"
+    },
+    {
+        day: '3',
+        date: "2023-09-15"
+    }
+]
+
+export const october = [
+    {
+        day: '1',
+        date: "2023-09-13"
+    },
+    {
+        day: '2',
+        date: "2023-09-14"
+    },
+    {
+        day: '3',
+        date: "2023-09-15"
+    }
+]
+
+export const november = [
+    {
+        day: '1',
+        date: "2023-09-13"
+    },
+    {
+        day: '2',
+        date: "2023-09-14"
+    },
+    {
+        day: '3',
+        date: "2023-09-15"
+    }
+]
+
+export const december = [
+    {
+        day: '1',
+        date: "2023-09-13"
+    },
+    {
+        day: '2',
+        date: "2023-09-14"
+    },
+    {
+        day: '3',
+        date: "2023-09-15"
+    }
+]
