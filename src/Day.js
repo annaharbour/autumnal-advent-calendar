2b94de2eda1ee9a0be58d7df6fa4854b4eb35b5c
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-09-14 21:58:21 -0400

    added arrays of other months

--- src/Day.js
+++ src/Day.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 // Passing day from Calendar where we're mapping through the days array
 function Day({day}){
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  
+  //Event handler for click setting the card to flipped and setting the backside of the car to the image in the days object
+  const flipCard = () => {
+    setIsFlipped(!isFlipped);
+  }
+
 // destructuring date out of each of the day objects
-  const {date} = day
+  const {date, image} = day
 
   return (
     // Rendering the day
-    <div className='day'>Day {date}</div>
+    <div className={`day ${isFlipped? 'flipped' : ''}`} onClick={flipCard}>
+      <div className='front'>
+        Day {date}
+      </div>
+      <div className='back' style={{backgroundImage: `url(${image})`}}>
+        
+      </div>
+    </div>
   )
 }
 
--- src/days.js
+++ src/days.js
@@ -2,75 +2,564 @@
 
 export const days = [
     {
-        day: '1',
-        date: "2023-09-13"
+        day: '22',
+        date: "09/22",
+        image: '/img/september/acro-yoga-park.jpg',
+        event: 'Festival in the Park'
     },
     {
-        day: '2',
-        date: "2023-09-14"
-    },
-    {
-        day: '3',
-        date: "2023-09-15"
-    }
-]
+        day: '23',
+        date: "09/23",
+        image: '/img/september/popcorn.jpg',
+        event: 'Festival in the Park'
 
-export const september = [
-    {
-        day: '1',
-        date: "2023-09-13"
     },
     {
-        day: '2',
-        date: "2023-09-14"
+        day: '24',
+        date: "09/24",
+        image: '/img/september/vintage-market.jpg',
+        event: 'Optimist Hall Vintage Market 11am-8pm'
     },
     {
-        day: '3',
-        date: "2023-09-15"
-    }
+        day: '25',
+        date: "09/25",
+        image: '/img/september/chai-tea.jpg',
+        event: 'Mondays Deserve Chai'
+    },
+    {
+        day: '26',
+        date: "09/26",
+        image: '/img/september/fall-kitten.jpg',
+        event: ''
+    },
+    {
+        day: '27',
+        date: "09/27",
+        image: '/img/september/sunflower.jpg',
+        event: 'Sunflowers are in Season. Check out the market at Northend'
+    },
+    {
+        day: '28',
+        date: "09/28",
+        image: '/img/september/pumpkin-latte.jpg',
+        event: 'Fall Drinks BOGO at Starbucks'
+    },
+    {
+        day: '29',
+        date: "09/29",
+        image: '/img/september/zombieland-arsley.jpg',
+        event: 'Zombieland and Ghostbusters Playing at Arlsey Retro Cinema from 9/29 to 10/5'
+    },
+    {
+        day: '30',
+        date: "09/30",
+        image: '/img/september/food-truck.jpg',
+        event: 'Charlotte Veg Fest'
+    },
+
 ]
 
 export const october = [
     {
         day: '1',
-        date: "2023-09-13"
+        date: "10/01",
+        image: '/img/october/hike.jpg',
+        event: 'Sol Siesta, Hiking, and Apple Picking'
     },
     {
         day: '2',
-        date: "2023-09-14"
+        date: "10/02",
+        image: '/img/october/halloween-cold-brew.jpg',
+        event: 'Mondays Deserve Coffee'
     },
     {
         day: '3',
-        date: "2023-09-15"
+        date: "10/03",
+        image: '/img/october/scary-book.jpg',
+        event: 'Time to start a scary book'
+    },
+    {
+        day: '4',
+        date: "10/04",
+        image: '/img/october/cousin-it.jpg',
+        event: ''
+    },
+    {
+        day: '5',
+        date: "10/05",
+        image: '/img/october/ghostbusters-arlsey.jpg',
+        event: 'Last Day for Ghostbusters and Zombieland at Arlsey Cinema'
+    },
+    {
+        day: '6',
+        date: "10/06",
+        image: '/img/october/scary-clown.jpg',
+        event: 'Scarowinds'
+    },
+    {
+        day: '7',
+        date: "10/07",
+        image: '/img/october/haunted-amusement-park.jpg',
+        event: 'Scarowinds'
+    },
+    {
+        day: '8',
+        date: "10/08",
+        image: '/img/october/halloween-costume.jpg',
+        event: 'Pick a costume?'
+    },
+    {
+        day: '9',
+        date: "10/09",
+        image: '/img/october/pumpkin-mug.jpg',
+        event: 'Mondays deserve coffee'
+    },
+    {
+        day: '10',
+        date: "10/10",
+        image: '/img/october/candy.jpg',
+        event: ''
+    },
+    {
+        day: '11',
+        date: "10/11",
+        image: '/img/october/jason-mask.jpg',
+        event: 'Scary Movie'
+    },
+    {
+        day: '12',
+        date: "10/12",
+        image: '/img/october/nightmare-before-xmas.jpg',
+        event: 'Camp Northend: Nightmare Before Christmas'
+    },
+    {
+        day: '13',
+        date: "10/13",
+        image: '/img/october/friday-the-13th.jpg',
+        event: 'Friday the 13th'
+    },
+    {
+        day: '14',
+        date: "10/14",
+        image: '/img/october/hogwarts-ticket.jpg',
+        event: 'Nebels Alley Night Market Harry Potter Theme'
+    },
+    {
+        day: '15',
+        date: "10/15",
+        image: '/img/october/cousin-it.jpg',
+        event: 'Spooky Movie night?'
+    },
+    {
+        day: '16',
+        date: "10/16",
+        image: '/img/october/halloween-decorations.jpg',
+        event: ''
+    },
+    {
+        day: '17',
+        date: "10/17",
+        image: '/img/october/harry-potter-train.jpg',
+        event: 'Harry Potter Movie Night?'
+    },
+    {
+        day: '18',
+        date: "10/18",
+        image: '/img/october/halloween-macaroons.jpg',
+        event: 'Check out Amelies Fall Menu'
+    },
+    {
+        day: '19',
+        date: "10/19",
+        image: '/img/october/cauldron.jpg',
+        event: 'Camp Northend: Hocus Pocus'
+    },
+    {
+        day: '20',
+        date: "10/20",
+        image: '/img/october/ .jpg',
+        event: 'Scream and Halloween Playing at Arsley Cinema from 20-26th'
+    },
+    {
+        day: '21',
+        date: "10/21",
+        image: '/img/october/pumpkin-patch.jpg',
+        event: 'Pumpkin Picking & Scarrigan Farms'
+    },
+    {
+        day: '22',
+        date: "10/22",
+        image: '/img/october/jack-o-lantern.jpg',
+        event: 'Pumpkin Carving and Movie Night'
+    },
+    {
+        day: '23',
+        date: "10/23",
+        image: '/img/october/headless-barista.jpg',
+        event: 'Mondays deserve coffee'
+    },
+    {
+        day: '24',
+        date: "10/24",
+        image: '/img/october/black-cat.jpg',
+        event: ''
+    },
+    {
+        day: '25',
+        date: "10/25",
+        image: '/img/october/cousin-it.jpg',
+        event: ''
+    },
+    {
+        day: '26',
+        date: "10/26",
+        image: '/img/october/rocky-horror.jpg',
+        event: 'Camp Northend: Rocky Horror Picture Show'
+    },
+    {
+        day: '27',
+        date: "10/27",
+        image: '/img/october/apple-cider.jpg',
+        event: 'Make your dish or drink for the party!'
+    },
+    {
+        day: '28',
+        date: "10/28",
+        image: '/img/october/halloween-string-lights.jpg',
+        event: `Sophia's Halloween Party`
+    },
+    {
+        day: '29',
+        date: "10/29",
+        image: '/img/october/dog-ghost.jpg',
+        event: ''
+    },
+    {
+        day: '30',
+        date: "10/30",
+        image: '/img/october/candy-corn.jpg',
+        event: 'Better buy candy'
+    },
+    {
+        day: '31',
+        date: "10/31",
+        image: '/img/october/trick-or-treater.jpg',
+        event: 'Trick or treat!'
     }
 ]
 
 export const november = [
     {
         day: '1',
-        date: "2023-09-13"
+        date: "11/01"
     },
     {
         day: '2',
-        date: "2023-09-14"
+        date: "11/02"
     },
     {
         day: '3',
-        date: "2023-09-15"
+        date: "11/03"
+    },
+    {
+        day: '4',
+        date: "11/04"
+    },
+    {
+        day: '5',
+        date: "11/05"
+    },
+    {
+        day: '6',
+        date: "11/06"
+    },
+    {
+        day: '7',
+        date: "11/07"
+    },
+    {
+        day: '8',
+        date: "11/08"
+    },
+    {
+        day: '9',
+        date: "11/09"
+    },
+    {
+        day: '10',
+        date: "11/10"
+    },
+    {
+        day: '11',
+        date: "11/11"
+    },
+    {
+        day: '12',
+        date: "11/12"
+    },
+    {
+        day: '13',
+        date: "11/13"
+    },
+    {
+        day: '14',
+        date: "11/14"
+    },
+    {
+        day: '15',
+        date: "11/15"
+    },
+    {
+        day: '16',
+        date: "11/16"
+    },
+    {
+        day: '17',
+        date: "11/17"
+    },
+    {
+        day: '18',
+        date: "11/18"
+    },
+    {
+        day: '19',
+        date: "11/19"
+    },
+    {
+        day: '20',
+        date: "11/20"
+    },
+    {
+        day: '21',
+        date: "11/21"
+    },
+    {
+        day: '22',
+        date: "11/22"
+    },
+    {
+        day: '23',
+        date: "11/23"
+    },
+    {
+        day: '24',
+        date: "11/24"
+    },
+    {
+        day: '25',
+        date: "11/25"
+    },
+    {
+        day: '26',
+        date: "11/26"
+    },
+    {
+        day: '27',
+        date: "11/27"
+    },
+    {
+        day: '28',
+        date: "11/28"
+    },
+    {
+        day: '29',
+        date: "11/29"
+    },
+    {
+        day: '30',
+        date: "11/30"
+    },
+    {
+        day: '31',
+        date: "11/31"
     }
 ]
 
 export const december = [
     {
         day: '1',
-        date: "2023-09-13"
+        date: "10/01",
+        image: '/img/december/.jpg',
+        event: ''
     },
     {
         day: '2',
-        date: "2023-09-14"
+        date: "10/02",
+        image: '/img/december/.jpg',
+        event: ''
     },
     {
         day: '3',
-        date: "2023-09-15"
+        date: "10/03",
+        image: '/img/december/.jpg',
+        event: ''
+    },
+    {
+        day: '4',
+        date: "10/04",
+        image: '/img/december/ .jpg',
+        event: ''
+    },
+    {
+        day: '5',
+        date: "10/05",
+        image: '/img/december/.jpg',
+        event: ''
+    },
+    {
+        day: '6',
+        date: "10/06",
+        image: '/img/december/.jpg',
+        event: ''
+    },
+    {
+        day: '7',
+        date: "10/07",
+        image: '/img/december/.jpg',
+        event: ''
+    },
+    {
+        day: '8',
+        date: "10/08",
+        image: '/img/december/.jpg',
+        event: ''
+    },
+    {
+        day: '9',
+        date: "10/09",
+        image: '/img/december.jpg',
+        event: ''
+    },
+    {
+        day: '10',
+        date: "10/10",
+        image: '/img/december.jpg',
+        event: ''
+    },
+    {
+        day: '11',
+        date: "10/11",
+        image: '/img/december.jpg',
+        event: 'Scary Movi'
+    },
+    {
+        day: '12',
+        date: "10/12",
+        image: '/img/december.jpg',
+        event: ''
+    },
+    {
+        day: '13',
+        date: "10/13",
+        image: '/img/december/.jpg',
+        event: ''
+    },
+    {
+        day: '14',
+        date: "10/14",
+        image: '/img/december/.jpg',
+        event: ''
+    },
+    {
+        day: '15',
+        date: "10/15",
+        image: '/img/december/.jpg',
+        event: 'Spooky Movie night?'
+    },
+    {
+        day: '16',
+        date: "10/16",
+        image: '/img/december/.jpg',
+        event: ''
+    },
+    {
+        day: '17',
+        date: "10/17",
+        image: '/img/december/.jpg',
+        event: 'Harry Potter Movie Night?'
+    },
+    {
+        day: '18',
+        date: "10/18",
+        image: '/img/december/.jpg',
+        event: 'Check out Amelies Fall Menu'
+    },
+    {
+        day: '19',
+        date: "10/19",
+        image: '/img/december/.jpg',
+        event: 'Camp Northend: Hocus Pocus'
+    },
+    {
+        day: '20',
+        date: "10/20",
+        image: '/img/october/ .jpg',
+        event: 'Scream and Halloween Playing at Arsley Cinema from 20-26th'
+    },
+    {
+        day: '21',
+        date: "10/21",
+        image: '/img/october/pumpkin-patch.jpg',
+        event: 'Pumpkin Picking & Scarrigan Farms'
+    },
+    {
+        day: '22',
+        date: "10/22",
+        image: '/img/october/jack-o-lantern.jpg',
+        event: 'Pumpkin Carving and Movie Night'
+    },
+    {
+        day: '23',
+        date: "10/23",
+        image: '/img/october/headless-barista.jpg',
+        event: 'Mondays deserve coffee'
+    },
+    {
+        day: '24',
+        date: "10/24",
+        image: '/img/october/black-cat.jpg',
+        event: ''
+    },
+    {
+        day: '25',
+        date: "10/25",
+        image: '/img/october/cousin-it.jpg',
+        event: ''
+    },
+    {
+        day: '26',
+        date: "10/26",
+        image: '/img/october/rocky-horror.jpg',
+        event: 'Camp Northend: Rocky Horror Picture Show'
+    },
+    {
+        day: '27',
+        date: "10/27",
+        image: '/img/october/apple-cider.jpg',
+        event: 'Make your dish or drink for the party!'
+    },
+    {
+        day: '28',
+        date: "10/28",
+        image: '/img/october/halloween-string-lights.jpg',
+        event: `Sophia's Halloween Party`
+    },
+    {
+        day: '29',
+        date: "10/29",
+        image: '/img/october/dog-ghost.jpg',
+        event: ''
+    },
+    {
+        day: '30',
+        date: "10/30",
+        image: '/img/october/candy-corn.jpg',
+        event: 'Better buy candy'
+    },
+    {
+        day: '31',
+        date: "10/31",
+        image: '/img/october/trick-or-treater.jpg',
+        event: 'Trick or treat!'
     }
 ]
