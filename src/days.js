f5bdc65ed42ba8b72d7c1e4df842cd95d4aad113
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-09-17 14:59:57 -0400

    Added weekdays

--- src/days.js
+++ src/days.js
@@ -5,56 +5,64 @@ export const september = [
         day: '22',
         date: "09/22",
         image: '/img/september/hogwarts-ticket.jpg',
-        event: 'Nebels Alley Night Market 5:30-9:30pm'
+        event: 'Nebels Alley Night Market 5:30-9:30pm',
+        weekday: 'Friday'
     },
     {
         day: '23',
         date: "09/23",
         image: '/img/september/acro-yoga-park.jpg',
-        event: 'Festival in the Park and First Day of Autumn!'
-
+        event: 'Festival in the Park and First Day of Autumn!',
+        weekday: 'Saturday'
     },
     {
         day: '24',
         date: "09/24",
         image: '/img/september/vintage-market.jpg',
-        event: 'Optimist Hall Vintage Market 11am-8pm'
+        event: 'Optimist Hall Vintage Market 11am-8pm',
+        weekday: 'Sunday'
     },
     {
         day: '25',
         date: "09/25",
         image: '/img/september/chai-tea.jpg',
-        event: 'Mondays Deserve Chai'
+        event: 'Mondays Deserve Chai',
+        weekday: 'Monday'
     },
     {
         day: '26',
         date: "09/26",
         image: '/img/september/fall-kitten.jpg',
-        event: ''
+        event: '',
+        weekday: 'Tuesday'
     },
     {
         day: '27',
         date: "09/27",
         image: '/img/september/sunflower.jpg',
-        event: 'Sunflowers are in Season. Check out the market at Northend'
+        event: 'Sunflowers are in Season. Check out the market at Northend',
+        weekday: 'Wednesday'
     },
     {
         day: '28',
         date: "09/28",
         image: '/img/september/pumpkin-latte.jpg',
-        event: 'Fall Drinks BOGO at Starbucks'
+        event: 'Fall Drinks BOGO at Starbucks',
+        weekday: 'Thursday'
     },
     {
         day: '29',
         date: "09/29",
         image: '/img/september/zombieland-arsley.jpg',
-        event: 'Zombieland and Ghostbusters Playing at Arlsey Retro Cinema from 9/29 to 10/5'
+        event: 'Zombieland and Ghostbusters Playing at Arlsey Retro Cinema from 9/29 to 10/5',
+        weekday: 'Friday'
     },
     {
         day: '30',
         date: "09/30",
         image: '/img/september/food-truck.jpg',
-        event: 'Charlotte Veg Fest'
+        event: 'Charlotte Veg Fest',
+        weekday: 'Saturday'
     },
 
 ]
@@ -64,314 +72,373 @@ export const october = [
         day: '1',
         date: "10/01",
         image: '/img/october/hike.jpg',
-        event: 'Sol Siesta, Hiking, and Apple Picking'
+        event: 'Sol Siesta, Hiking, and Apple Picking',
+        weekday: 'Sunday'
     },
     {
         day: '2',
         date: "10/02",
         image: '/img/october/halloween-cold-brew.jpg',
-        event: 'Mondays Deserve Coffee'
+        event: 'Mondays Deserve Coffee',
+        weekday: 'Monday'
     },
     {
         day: '3',
         date: "10/03",
         image: '/img/october/scary-book.jpg',
-        event: 'Time to start a scary book'
+        event: 'Time to start a scary book',
+        weekday: 'Tuesday'
     },
     {
         day: '4',
         date: "10/04",
         image: '/img/october/autumn-park.jpg',
-        event: 'Crunchy fall leaves are the best'
+        event: 'Crunchy fall leaves are the best',
+        weekday: 'Wednesday'
     },
     {
         day: '5',
         date: "10/05",
         image: '/img/october/ghostbusters-arsley.jpg',
-        event: 'Last Day for Ghostbusters and Zombieland at Arlsey Cinema'
+        event: 'Last Day for Ghostbusters and Zombieland at Arlsey Cinema',
+        weekday: 'Thursday'
     },
     {
         day: '6',
         date: "10/06",
         image: '/img/october/scary-clown.jpg',
-        event: 'Scarowinds'
+        event: 'Scarowinds',
+        weekday: 'Friday'
     },
     {
         day: '7',
         date: "10/07",
         image: '/img/october/haunted-amusement-park.jpg',
-        event: 'Scarowinds'
+        event: 'Scarowinds',
+        weekday: 'Saturday'
     },
     {
         day: '8',
         date: "10/08",
         image: '/img/october/jason-mask.jpg',
-        event: 'Get cozy with a sary movie'
+        event: 'Get cozy with a scary movie',
+        weekday: 'Sunday'
     },
     {
         day: '9',
         date: "10/09",
         image: '/img/october/pumpkin-mug.jpg',
-        event: 'Mondays deserve coffee'
+        event: 'Mondays deserve coffee',
+        weekday: 'Monday'
     },
     {
         day: '10',
         date: "10/10",
         image: '/img/october/candy.jpg',
-        event: 'Resist the urge to buy candy early... you know what will happen.'
+        event: 'Resist the urge to buy candy early... you know what will happen.',
+        weekday: 'Tuesday'
     },
     {
         day: '11',
         date: "10/11",
         image: '/img/october/halloween-costume.jpg',
-        event: `Do you know what you're going as for halloween?`
+        event: `Do you know what you're going as for halloween?`,
+        weekday: 'Wednesday'
     },
     {
         day: '12',
         date: "10/12",
         image: '/img/october/nightmare-before-xmas.jpg',
-        event: 'Camp Northend: Nightmare Before Christmas'
+        event: 'Camp Northend: Nightmare Before Christmas',
+        weekday: 'Thursday'
     },
     {
         day: '13',
         date: "10/13",
         image: '/img/october/friday-the-13th.jpg',
-        event: 'Friday the 13th'
+        event: 'Friday the 13th',
+        weekday: 'Friday'
     },
     {
         day: '14',
         date: "10/14",
         image: '/img/october/hike.jpg',
-        event: 'Whitewater Center Boat Building Competition'
+        event: 'Whitewater Center Boat Building Competition',
+        weekday: 'Saturday'
     },
     {
         day: '15',
         date: "10/15",
         image: '/img/october/uncle-fester.jpg',
-        event: 'We Gladly feast on those who would subdue us'
+        event: 'We Gladly feast on those who would subdue us',
+        weekday: 'Sunday'
     },
     {
         day: '16',
         date: "10/16",
         image: '/img/october/halloween-decorations.jpg',
-        event: 'Go for a drive and check out the decorations'
+        event: 'Go for a drive and check out the decorations',
+        weekday: 'Monday'
     },
     {
         day: '17',
         date: "10/17",
         image: '/img/october/harry-potter-train.jpg',
-        event: 'If you start now you can finish all the Harry Potter movies before Halloween'
+        event: 'If you start now you can finish all the Harry Potter movies before Halloween',
+        weekday: 'Tuesday'
     },
     {
         day: '18',
         date: "10/18",
         image: '/img/october/halloween-macaroons.jpg',
-        event: 'Check out Amelies Fall Macaroons'
+        event: 'Check out Amelies Fall Macaroons',
+        weekday: 'Wednesday'
     },
     {
         day: '19',
         date: "10/19",
         image: '/img/october/cauldron.jpg',
-        event: 'Camp Northend: Hocus Pocus'
+        event: 'Camp Northend: Hocus Pocus',
+        weekday: 'Thursday'
     },
     {
         day: '20',
         date: "10/20",
         image: '/img/october/popcorn.jpg',
-        event: 'Scream and Halloween Playing at Arsley Cinema from 20-26th'
+        event: 'Scream and Halloween Playing at Arsley Cinema from 20-26th',
+        weekday: 'Friday'
     },
     {
         day: '21',
         date: "10/21",
         image: '/img/october/pumpkin-patch.jpg',
-        event: 'Pumpkin Picking & Scarrigan Farms'
+        event: 'Pumpkin Picking & Scarrigan Farms',
+        weekday: 'Saturday'
     },
     {
         day: '22',
         date: "10/22",
         image: '/img/october/jack-o-lantern.jpg',
-        event: 'Pumpkin Carving and Movie Night'
+        event: 'Time to carve dem pumpkins',
+        weekday: 'Sunday'
     },
     {
         day: '23',
         date: "10/23",
         image: '/img/october/headless-barista.jpg',
-        event: 'Mondays deserve coffee'
+        event: 'Mondays deserve coffee',
+        weekday: 'Monday'
     },
     {
         day: '24',
         date: "10/24",
         image: '/img/october/black-cat.jpg',
-        event: 'Bad Luck'
+        event: 'Bad Luck',
+        weekday: 'Tuesday'
     },
     {
         day: '25',
         date: "10/25",
         image: '/img/october/cousin-it.jpg',
-        event: 'It Girl'
+        event: 'It Girl',
+        weekday: 'Wednesday'
     },
     {
         day: '26',
         date: "10/26",
         image: '/img/october/rocky-horror.jpg',
-        event: 'Camp Northend: Rocky Horror Picture Show'
+        event: 'Camp Northend: Rocky Horror Picture Show',
+        weekday: 'Thursday'
     },
     {
         day: '27',
         date: "10/27",
         image: '/img/october/apple-cider.jpg',
-        event: 'Time to brew something spooktacular!'
+        event: 'Time to brew something spooktacular!',
+        weekday: 'Friday'
     },
     {
         day: '28',
         date: "10/28",
         image: '/img/october/halloween-string-lights.jpg',
-        event: `Halloween Parties are Booming`
+        event: `Halloween Parties are Booming`,
+        weekday: 'Saturday'
     },
     {
         day: '29',
         date: "10/29",
         image: '/img/october/dog-ghost.jpg',
-        event: 'Hair of the Dog?'
+        event: 'Hair of the Dog?',
+        weekday: 'Sunday'
     },
     {
         day: '30',
         date: "10/30",
         image: '/img/october/candy-corn.jpg',
-        event: 'Better buy candy'
+        event: 'Better buy candy',
+        weekday: 'Monday'
     },
     {
         day: '31',
         date: "10/31",
         image: '/img/october/trick-or-treater.jpg',
-        event: 'Trick or treat!'
+        event: 'Trick or treat!',
+        weekday: 'Tuesday'
     }
 ]
 
 export const november = [
     {
         day: '1',
-        date: "11/01"
+        date: "11/01",
+        weekday: 'Wednesday'
     },
     {
         day: '2',
-        date: "11/02"
+        date: "11/02",
+        weekday: 'Thursday'
     },
     {
         day: '3',
-        date: "11/03"
+        date: "11/03",
+        weekday: 'Friday'
     },
     {
         day: '4',
-        date: "11/04"
+        date: "11/04",
+        weekday: 'Saturday'
     },
     {
         day: '5',
-        date: "11/05"
+        date: "11/05",
+        weekday: 'Sunday'
     },
     {
         day: '6',
-        date: "11/06"
+        date: "11/06",
+        weekday: 'Monday'
     },
     {
         day: '7',
-        date: "11/07"
+        date: "11/07",
+        weekday: 'Tuesday'
     },
     {
         day: '8',
-        date: "11/08"
+        date: "11/08",
+        weekday: 'Wednesday'
     },
     {
         day: '9',
-        date: "11/09"
+        date: "11/09",
+        weekday: 'Thursday'
     },
     {
         day: '10',
-        date: "11/10"
+        date: "11/10",
+        weekday: 'Friday'
     },
     {
         day: '11',
-        date: "11/11"
+        date: "11/11",
+        weekday: 'Saturday'
     },
     {
         day: '12',
-        date: "11/12"
+        date: "11/12",
+        weekday: 'Sunday'
     },
     {
         day: '13',
-        date: "11/13"
+        date: "11/13",
+        weekday: 'Monday'
     },
     {
         day: '14',
-        date: "11/14"
+        date: "11/14",
+        weekday: 'Tuesday'
     },
     {
         day: '15',
-        date: "11/15"
+        date: "11/15",
+        weekday: 'Wednesday'
     },
     {
         day: '16',
-        date: "11/16"
+        date: "11/16",
+        weekday: 'Thursday'
+        
     },
     {
         day: '17',
-        date: "11/17"
+        date: "11/17",
+        weekday: 'Friday'
     },
     {
         day: '18',
-        date: "11/18"
+        date: "11/18",
+        weekday: 'Saturday'
     },
     {
         day: '19',
-        date: "11/19"
+        date: "11/19",
+        weekday: 'Sunday'
     },
     {
         day: '20',
-        date: "11/20"
+        date: "11/20",
+        weekday: 'Monday'
     },
     {
         day: '21',
-        date: "11/21"
+        date: "11/21",
+        weekday: 'Tuesday'
     },
     {
         day: '22',
-        date: "11/22"
+        date: "11/22",
+        event: 'Ice Skating and Lights Open at US National White Water Center',
+        weekday: 'Wednesday'
     },
     {
         day: '23',
-        date: "11/23"
+        date: "11/23",
+        weekday: 'Thursday'
     },
     {
         day: '24',
-        date: "11/24"
+        date: "11/24",
+        weekday: 'Friday'
     },
     {
         day: '25',
-        date: "11/25"
+        date: "11/25",
+        weekday: 'Saturday'
     },
     {
         day: '26',
-        date: "11/26"
+        date: "11/26",
+        weekday: 'Sunday'
     },
     {
         day: '27',
-        date: "11/27"
+        date: "11/27",
+        weekday: 'Monday'
     },
     {
         day: '28',
-        date: "11/28"
+        date: "11/28",
+        weekday: 'Tuesday'
     },
     {
         day: '29',
-        date: "11/29"
+        date: "11/29",
+        weekday: 'Wednesday'
     },
     {
         day: '30',
-        date: "11/30"
-    },
-    {
-        day: '31',
-        date: "11/31"
+        date: "11/30",
+        weekday: 'Thursday'
     }
 ]
 
