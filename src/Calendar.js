c339997d08de2e0b8a9d6c5b9f13072372785796
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-09-17 19:06:08 -0400

    calendar sorting by day

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,38 +1,156 @@
-import React, {useState} from 'react'
-import Day from './Day'
-import { september, october, november} from './days'
+// import React, {useState} from 'react'
+// import Day from './Day'
+// import { september, october, november} from './days'
+
+// function Calendar() {
+//   const [currentMonth, setCurrentMonth] = useState('September');
+//   const months = {
+//     September: september,
+//     October: october,
+//     November: november
+//   }
+
+//   // Setting new month when button is pressed
+//   const changeMonth = (newMonth) => {
+//     setCurrentMonth(newMonth);
+//   }
+// // Defining whether or not there is a previous or next month
+//   const monthKeys = Object.keys(months);
+//   const currentIndex = monthKeys.indexOf(currentMonth);
+//   const hasNextMonth = currentIndex < monthKeys.length - 1;
+//   const hasPreviousMonth = currentIndex > 0;
+
+//   // Conditionally setting background class to gourds instead of leaves when the month is November
+//   const backgroundClass = currentMonth === 'November' ? 'background-november' : 'background';
+  
+//   // Group the days by day of the week
+//   const daysByWeekday = {
+//     Sunday: [],
+//     Monday: [],
+//     Tuesday: [],
+//     Wednesday: [],
+//     Thursday: [],
+//     Friday: [],
+//     Saturday: [],
+//   };
+
+//   // Setting date and weekday for each day
+//   months[currentMonth].forEach((day) => {
+//     const date = new Date(day.date);
+//     const weekday = date.toLocaleString('en-US', { weekday: 'long' });
+//     daysByWeekday[weekday].push(day);
+//   });
+
+//   // Define the order of days of the week
+//   const daysOfWeek = ['Saturday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+//    return (
+//     <div className={backgroundClass}>
+//       <div className='month'>
+//         {/* Prev Month Button */}
+//         <button
+//           className={`next ${!hasPreviousMonth ? 'disabled' : ''}`}
+//           onClick={() => hasPreviousMonth && changeMonth(monthKeys[currentIndex - 1])}
+//         >
+//           <i className="fa-solid fa-caret-left"></i>
+//         </button>
+//         {/* Current Month Header */}
+//         <h1>{currentMonth}</h1>
+//         {/* Next Month Button */}
+//         <button
+//           className={`next ${!hasNextMonth ? 'disabled' : ''}`}
+//           onClick={() => hasNextMonth && changeMonth(monthKeys[currentIndex + 1])}
+//         >
+//           <i className="fa-solid fa-caret-right"></i>
+//         </button>
+//       </div>
+// <div className='calendar'>
+// {daysOfWeek.map((dayOfWeek) => (
+//   <div className='week' key={dayOfWeek}>
+//     <div className='day-name'>
+//       {dayOfWeek.slice(0, 1)} {/* Display the first character */}
+//     </div>
+//     {months[currentMonth]
+//       .filter((day) => day.weekday === dayOfWeek)
+//       .sort((a, b) => (a.date < b.date ? -1 : 1)) // Sort by date
+//       .map((day) => (
+//         <Day key={day.date} day={day} />
+//       ))}
+//   </div>
+// ))}
+
+
+
+// </div>
+
+      
+      
+//     </div>
+//   )
+// }
+
+// export default Calendar
+
+import React, { useState } from 'react';
+import Day from './Day';
+import { september, october, november } from './days';
 
 function Calendar() {
   const [currentMonth, setCurrentMonth] = useState('September');
   const months = {
     September: september,
     October: october,
-    November: november
-  }
+    November: november,
+  };
 
+  // Setting new month when button is pressed
   const changeMonth = (newMonth) => {
     setCurrentMonth(newMonth);
-  }
+  };
 
+  // Defining whether or not there is a previous or next month
   const monthKeys = Object.keys(months);
   const currentIndex = monthKeys.indexOf(currentMonth);
   const hasNextMonth = currentIndex < monthKeys.length - 1;
   const hasPreviousMonth = currentIndex > 0;
 
+  // Conditionally setting background class to gourds instead of leaves when the month is November
   const backgroundClass = currentMonth === 'November' ? 'background-november' : 'background';
 
-   return (
-    // <div className='background'>
+  // Group the days by day of the week
+  const daysByWeekday = {
+    Sunday: [],
+    Monday: [],
+    Tuesday: [],
+    Wednesday: [],
+    Thursday: [],
+    Friday: [],
+    Saturday: [],
+  };
+
+  // Setting date and weekday for each day
+  months[currentMonth].forEach((day) => {
+    const date = new Date(day.date);
+    const weekday = date.toLocaleString('en-US', { weekday: 'long' });
+    daysByWeekday[weekday].push(day);
+  });
+
+  // Define the order of days of the week
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  return (
     <div className={backgroundClass}>
       <div className='month'>
+        {/* Prev Month Button */}
         <button
           className={`next ${!hasPreviousMonth ? 'disabled' : ''}`}
           onClick={() => hasPreviousMonth && changeMonth(monthKeys[currentIndex - 1])}
-          
         >
           <i className="fa-solid fa-caret-left"></i>
         </button>
+        {/* Current Month Header */}
         <h1>{currentMonth}</h1>
+        {/* Next Month Button */}
         <button
           className={`next ${!hasNextMonth ? 'disabled' : ''}`}
           onClick={() => hasNextMonth && changeMonth(monthKeys[currentIndex + 1])}
@@ -40,13 +158,22 @@ function Calendar() {
           <i className="fa-solid fa-caret-right"></i>
         </button>
       </div>
-    <div className='calendar'>
-        {months[currentMonth].map((day) => (
-          <Day key={day.day} day={day}/>
+      <div className='calendar'>
+        {daysOfWeek.map((dayOfWeek) => (
+          <div className='week' key={dayOfWeek}>
+            <div className='day-name'>
+              {dayOfWeek.slice(0, 1)} {/* Display the first character */}
+            </div>
+            {months[currentMonth]
+              .filter((day) => day.weekday === dayOfWeek)
+              .map((day) => (
+                <Day key={day.date} day={day} />
+              ))}
+          </div>
         ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Calendar
+export default Calendar;
